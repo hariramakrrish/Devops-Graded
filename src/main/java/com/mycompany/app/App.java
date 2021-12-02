59f659198a45291ed558278b47c240ce2016f5bb
HARIRAMAKRRISHNAN R <hariramakrrishnan.r@hcl.com>
2021-12-02 13:05:33 +0530

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	Scanner ui = new Scanner(System.in);
+        System.out.println( "Hello World!" );
+		System.out.println("Life is Good");
+int a;
+System.out.println("ENTER A Number");
+a=ui.nextInt();
+System.out.println(a);
+    }
+}
