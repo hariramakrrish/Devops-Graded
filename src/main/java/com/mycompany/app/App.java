43a6174ff38226285209ea472170838c6df99851
hariramakrrish <73615711+hariramakrrish@users.noreply.github.com>
2021-12-03 11:23:04 +0530

    changes 3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,16 @@
 package com.mycompany.app;
-
+import java.util.*;
 public class App 
 {
     public static void main( String[] args )
     {
+	Scanner ui = new Scanner(System.in);
         System.out.println( "Hello World!" );
 	System.out.println("Life is Good");
+	
+	    int a;
+	    System.out.priintln("Enter any int");
+	    a=ui.nextInt();
+	    System.out.println("the num is "+a);
     }
 }
